ad09e91cd77ffa3ec01d2651bd7115df14b7cf97
Javier2210 <josejavier_ga@hotmail.com>
2025-03-14 11:19:34 -0400

    carrito de compras.py

--- carrito de compras.py
+++ carrito de compras.py
@@ -1,6 +1,7 @@
-#Cesta de compras
+#Carrito de compras
 
 carrito_de_compras = []
+precio = []
 
 def mostrar_menu():
     print ('Menu')
@@ -15,35 +16,45 @@ while True:
     
     if opcion == "1":
        
-        print ('Ingresa 6 para regresar al menu anterior')
-        elemento = input("Introduce el nombre del elemento: ")
+        print (f'Ingresa 6 para regresar al menu anterior')
+        elemento = input(f"Ingresa el nombre del elemento: ")
+        
         if elemento == "6":
            print(mostrar_menu())
+           print(f"Regresaste al menu anterior")
         else:
+            precios = float(input(f"Ingresa el precio del producto: "))
             carrito_de_compras.append(elemento)
+            precio.append(precios)
             print(f"{elemento} ha sido agregado a la cesta.")
     
     elif opcion == "2":
       
-        print("Cesta de la compra:")
+        print("Carrito de la compra:")
         for i, item in enumerate(carrito_de_compras, start=1):
             print(f"{i}. {item}")
 
     elif opcion == "3":
         
         print (f'Ingresa 6 para regresar al menu anterior')
-        elemento_a_eliminar = input(f"Introduce el nombre del elemento a eliminar: ")
+        elemento_a_eliminar = input(f"Ingresa el nombre y precio del elemento a eliminar: ")
         if elemento_a_eliminar == "6":
             print(mostrar_menu())
+            print(f"Regresaste al menu anterior")
+
         elif elemento_a_eliminar in carrito_de_compras:
             carrito_de_compras.remove(elemento_a_eliminar)
+            precio_a_eliminar = float(input(f"Ingresa el precio a eliminar: "))
+            precio_a_eliminar in precio           
+            precio.remove(precio_a_eliminar)
             print(f"{elemento_a_eliminar} ha sido eliminado.")
         else:
-            print(f"{elemento_a_eliminar} no está en la cesta.")
+            print(f"{elemento_a_eliminar} no está en el carrito.")
 
     elif opcion == "4":
        
-        print(f"El total es {len(carrito_de_compras)} elemento(s).") 
+        print(f"El total es {len(carrito_de_compras)} elementos.") 
+        print(f'El precio total es de: {sum(precio)}$')
 
     elif opcion == "5":
      
